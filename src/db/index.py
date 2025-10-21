0ef4f685b43474927f23d1f975ebd6521b227cc4
unknown <1034942735@qq.com>
2025-10-21 22:57:24 +0800

    save

--- src/db/index.py
+++ src/db/index.py
@@ -4,7 +4,7 @@ db_name = ''
 db_url = f''
 engine = create_engine(db_url, echo=True)
 
-def create_db_tables():
+def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
 
 def db_session():
