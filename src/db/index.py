227ded66a3dabeda4f3b93cd2497a4d2456026a0
unknown <1034942735@qq.com>
2025-10-21 22:54:23 +0800

    save

---
+++ src/db/index.py
@@ -0,0 +1,15 @@
+from sqlmodel import create_engine, SQLModel, Session
+
+db_name = ''
+db_url = f''
+engine = create_engine(db_url, echo=True)
+
+def create_db_tables():
+    SQLModel.metadata.create_all(engine)
+
+def db_session():
+    session = Session(engine)
+    try:
+        yield session
+    finally:
+        session.close()
